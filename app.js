6ea13230996dae1d25c530e3555a5f717ed7a2b9
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-23 02:50:49 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -16,16 +16,73 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // route to base domain
-app.get("/", function(req, res){
-    res.render("home.ejs");
+app.get("/", async function(req, res){
+    res.render("home.ejs", {"data": await handleSearch("otter", "vertical") });
 });
 
+/*
 // route to base domain
-app.get("/home", function(req, res){
+app.get("/home", async function(req, res){
     res.render("home.ejs");
+});
+*/
+
+app.post("/home", async function(req, res){
+    console.log(req.body.searchTerm); 
+    console.log(req.body.orientation);
+    //var data = await handleSearch(req.body.searchTerm, req.body.orientation);
+    //console.log(data.img1.url);
+    res.render("home.ejs", {"data": await handleSearch(req.body.searchTerm, req.body.orientation)} );
 });
 
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function handleSearch(searchTerm, orientation){
+    var url = "https://pixabay.com/api/?";
+    url += "key=15450334-54c089ab058c7a8209a75c73b";
+    //searchTerm = searchTerm.replace(/ /g, '+');
+    url += "&q=" + searchTerm;
+    url += "&orientation=" + orientation;
+    url += "&image_type=photo";
+    url += "&per_page=200";
+    
+    return new Promise( function(resolve, reject){
+        request(url, function(error, response, body){
+            if ( !error && response.statusCode == 200 ){
+                let parsedData = JSON.parse(body);
+                
+                var picCount = parsedData.hits.length;
+                
+                resolve({
+                    img1: {
+                        url: parsedData.hits[getRandomInt(picCount-1)].largeImageURL,
+                        likes: parsedData.hits[getRandomInt(picCount-1)].likes
+                    },
+                    img2: {
+                        url: parsedData.hits[getRandomInt(picCount-1)].largeImageURL,
+                        likes: parsedData.hits[getRandomInt(picCount-1)].likes
+                    },
+                    img3: {
+                        url: parsedData.hits[getRandomInt(picCount-1)].largeImageURL,
+                        likes: parsedData.hits[getRandomInt(picCount-1)].likes
+                    },
+                    img4: {
+                        url: parsedData.hits[getRandomInt(picCount-1)].largeImageURL,
+                        likes: parsedData.hits[getRandomInt(picCount-1)].likes
+                    }
+                });
+            } 
+            else{
+                reject(error);
+                console.log(response.statusCode);
+                console.log(error);
+            }
+        });
+    });
+}
 
 // anything that hasn't matched a defined route is caught here
 app.get("/*", function(req, res){
